a58404b6ba5ca75179af6cb661e6194c911e3d3c
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-01-28 18:44:57 -0300

    feat(): MÉTODOS CRUD

--- jpaDemo/src/main/java/com/ejemplo/jpaDemo/Service/IPersonaService.java
+++ jpaDemo/src/main/java/com/ejemplo/jpaDemo/Service/IPersonaService.java
@@ -1,7 +1,23 @@
-
 package com.ejemplo.jpaDemo.Service;
 
+import com.ejemplo.jpaDemo.Model.Persona;
+import java.util.List;
 
 public interface IPersonaService {
-    
+    //metodos abstractos por ser interface
+
+    //método para traer todas las personas
+    public List<Persona> getPersonas();
+
+    //alta
+    public void savePersona(Persona perso);
+
+    //baja
+    public void deletePersona(Long id);
+
+    //lectura de un solo objeto
+    public Persona findPersona(Long id);
+
+    //edición/modificación
+    public void editPersona(Long idOriginal, Long idNueva, String nuevoNombre, String nuevoApellido, int nuevaEdad);
 }
--- jpaDemo/src/main/java/com/ejemplo/jpaDemo/Service/PersonaService.java
+++ jpaDemo/src/main/java/com/ejemplo/jpaDemo/Service/PersonaService.java
@@ -1,12 +1,54 @@
 
 package com.ejemplo.jpaDemo.Service;
 
+import com.ejemplo.jpaDemo.Model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ejemplo.jpaDemo.Repository.IPersonaRepository;
+import java.util.List;
 
 @Service
-public class PersonaService {
+public class PersonaService implements IPersonaService{
     @Autowired
-    private IPersonaRepository persoReposs;
+    private IPersonaRepository persoRepo;
+
+    @Override
+    public List<Persona> getPersonas() {
+        
+        List<Persona> listaPersonas = persoRepo.findAll();
+        return listaPersonas;
+    }
+
+    @Override
+    public void savePersona(Persona perso) {
+        
+        persoRepo.save(perso);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        persoRepo.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        
+        Persona perso = persoRepo.findById(id).orElse(null);
+        return perso;
+    }
+
+    @Override
+    public void editPersona(Long idOriginal, Long idNueva, String nuevoNombre, String nuevoApellido, int nuevaEdad) {
+        
+        //busco el pbjeto original
+        Persona perso = this.findPersona(idOriginal);
+        
+        perso.setId(idNueva);
+        perso.setNombre(nuevoNombre);
+        perso.setApellido(nuevoApellido);
+        perso.setEdad(nuevaEdad);
+        
+        //guardar los cambios
+        this.savePersona(perso);
+    }
 }

e9dc83bb8c65c3efd32f8d45eb9f55f81c57ab46
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-02-25 01:49:31 -0300

    feat(SecurityConfig): test Roles y Permisos nivel lógico

--- Example SpringSecurity/springSecurityBasic/src/main/java/com/example/springSecurityBasic/security/config/SecurityConfig.java
+++ Example SpringSecurity/springSecurityBasic/src/main/java/com/example/springSecurityBasic/security/config/SecurityConfig.java
@@ -1,25 +1,93 @@
 
 package com.example.springSecurityBasic.security.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                // AQUI está el cambio: Se abre un paréntesis y se usa "auth ->"
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/holanoseg").permitAll()
-                        .anyRequest().authenticated()
-                )
-                // Lo mismo para el login, se usa una lambda
-                .formLogin(form -> form.permitAll())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults()) //se usa cuando solo vas a logear con usuarios y contraseñas
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    // Endpoints públicos
+                    http.requestMatchers(HttpMethod.GET, "/holanoseg").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/holaseg").hasAuthority("READ");
+                    http.anyRequest().denyAll();
+                })
                 .build();
     }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        List userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("lucas")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("ADMIN")
+                .authorities("CREATE", "READ", "UPDATE", "DELETE")
+                .build());
+
+        userDetailsList.add(User.withUsername("seguidor")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("USER")
+                .authorities("READ")
+                .build());
+
+        userDetailsList.add(User.withUsername("actualizador")
+                .password("1234") // esto si no está codificado, sino, tiene que seguir el algoritmo de codificación
+                .roles("USER")
+                .authorities("UPDATE")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+
 }

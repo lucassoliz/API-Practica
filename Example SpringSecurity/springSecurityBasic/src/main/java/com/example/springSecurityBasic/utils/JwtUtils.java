330bd0480c1710033c2d341c261215cc6003bbb2
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-03-02 20:32:26 -0300

    feat(SecurityConfig): ADD met obtener name user del token + met obtener claims

--- Example SpringSecurity/springSecurityBasic/src/main/java/com/example/springSecurityBasic/utils/JwtUtils.java
+++ Example SpringSecurity/springSecurityBasic/src/main/java/com/example/springSecurityBasic/utils/JwtUtils.java
@@ -3,6 +3,7 @@ package com.example.springSecurityBasic.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -73,6 +75,24 @@ public DecodedJWT validateToken(String token) {
         throw new JWTVerificationException("Invalid token. Not authorized");
     }
 }
+//===========================================================================
+    //devuelvo el subject del token, que es el username, a partir del token decodificado
+public String extractUsername (DecodedJWT decodedJWT) {
+    //el subject es el usuario según establecimos al crear el token
+    return decodedJWT.getSubject().toString();
+}
+
+//===========================================================================
+//devuelvo un claim en particular a partir del token decodificado, por ejemplo el claim de authorities, que es el que contiene los permisos del usuario
+public Claim getSpecificClaim (DecodedJWT decodedJWT, String claimName) {
+    return decodedJWT.getClaim(claimName);
+}
+
+    //devuelvo todos los claims del token decodificado, que es un mapa de String (nombre del claim) y Claim (valor del claim), para poder acceder a todos los datos que contiene el token
+    public Map<String, Claim> returnAllClaims (DecodedJWT decodedJWT){
+        return decodedJWT.getClaims();
+    }
+
 
 
 
